48a2fc8abf21f9e662791869632990d0c872ad3b
rusanaklt <rusana.klt@gmail.com>
2023-08-13 22:12:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,32 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsArray = [];
+    var memberSkillsArrayText = [];
+    var memberSkillsArrayValue = [];
+    var memberSkillsArrayTextValue = [];
+    var memberSkillsArrayTextValueText = [];
+    var memberSkillsArrayTextValueValue = [];
+    var memberSkillsArrayTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValue = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueTextValueText = [];
+    var memberSkillsArrayTextValueTextValueTextValueTextValueTextValueText
